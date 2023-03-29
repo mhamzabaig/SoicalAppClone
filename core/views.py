2d337cbdcb3f151f33ca7a46fadf8dcce70f9f59
mhamzabaig <mhamzabaig1234@gmail.com>
2023-03-29 19:00:22 +0500

    Sign up Function added completely

--- core/views.py
+++ core/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+from .models import Profile
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
 
 def signup(request):
-    return render(request,'signup.html')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,"Email alread existed ")
+                redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,"Username alread existed ")
+                redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                # Log user in
+                # Create a new Profile
+                user_model = User.objects.get(username=username)
+                newProfile = Profile.objects.create(user=user_model,id_user=user_model.id)
+                newProfile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'Password not Matching ')
+            return redirect('signup')
+    else: return render(request,'signup.html')
