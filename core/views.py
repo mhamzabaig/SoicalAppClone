db65482ed51d05755c93eebab28d3b3db4c8d67c
mhamzabaig <mhamzabaig1234@gmail.com>
2023-04-03 18:06:24 +0500

    User Feed added

--- core/views.py
+++ core/views.py
@@ -4,16 +4,30 @@ from django.contrib import messages
 from .models import Profile,Post,LikePost,FollowersCount
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from itertools import chain
 # Create your views here.
 
 @login_required(login_url='/signin')
 def index(request):
-
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
+    
+    user_following_list = []
+    feed = []
+
+    user_following = FollowersCount.objects.filter(follower=request.user.username)
+    for users in user_following:
+        user_following_list.append(users.user)
+    
+    for usernames in user_following_list:
+        feed_lists = Post.objects.filter(user=usernames)
+        feed.append(feed_lists)
+
+    feed_list = list(chain(*feed))
+
     posts = Post.objects.all()
     
-    return render(request,'index.html',{'user_profile':user_profile,'posts':posts})
+    return render(request,'index.html',{'user_profile':user_profile,'posts':feed_list})
 
 def like_post(request):
     username = request.user.username
